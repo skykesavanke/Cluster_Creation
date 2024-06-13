0cbf58fa4a1a063f9ec15a5d107a124ec1a3a198
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-13 11:19:35 +0530

    modified the Jenkinsfile

--- variable.tf
+++ variable.tf
@@ -1,25 +1,27 @@
 variable "aws_region" {
   description = "This describe the region"
   type        = string
-  
+  default = "us-east-1"
 
 }
 variable "ecr_repo" {
   description = "This defines the repository"
   type        = string
+  default = "new-cluster-repo"
   
 
 }
 variable "aws_eks_cluster" {
   description = "This defines the eks cluster"
   type        = string
+  default = "new-cluster"
   
 
 }
 variable "aws_eks_node_group" {
   description = "This defines the eks cluster"
   type        = string
-  default     = "node-group"
+  default     = "new-cluster-node-grp"
 
 }
 
--- variables.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region ="us-east-1"
-ecr_repo="new-cluster-repo"
-aws_eks_cluster = "new-cluster"
-aws_eks_node_group = "new-cluster-node-grp"
