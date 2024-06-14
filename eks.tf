e9c09fb5ebcf61b1efd8101a924096b1145c0849
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-14 10:33:59 +0530

    updated the eks.tf --instance type

--- eks.tf
+++ eks.tf
@@ -15,7 +15,7 @@ resource "aws_eks_node_group" "example-group" {
   node_role_arn   = "arn:aws:iam::211125415675:role/my-role-1"
   subnet_ids      = ["subnet-03a43d27e21e45144", "subnet-0584d45f2b91de367"]
   instance_types =["t2.micro"] 
-  disk_size = 2
+  disk_size = 4
   scaling_config {
     desired_size = 1
     max_size     = 2
