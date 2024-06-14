6f11e788aad1929baa92ed878ff3239a74d71581
skykesavanke <keerthana.kesavan.ciec@skytv.it>
2024-06-14 12:14:00 +0530

    changed the instance type

--- eks.tf
+++ eks.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "example-group" {
   node_group_name = var.aws_eks_node_group
   node_role_arn   = "arn:aws:iam::211125415675:role/my-role-1"
   subnet_ids      = ["subnet-03a43d27e21e45144", "subnet-0584d45f2b91de367"]
-  instance_types =["t3.medium"] 
+ 
   
   scaling_config {
     desired_size = 1
